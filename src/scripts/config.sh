cdb7938d0d64d0c6ce3b2bf293ab802fb4926a62
Ben Letchford <ben_letchford@debortoli.com.au>
2019-01-24 17:11:54 +1100

    Cleanup

--- src/scripts/config.sh
+++
@@ -1,9 +0,0 @@
-# Setup standard Republic Parameters for use in other runs.
-export REPUBLIC_VERSION=0.6.2
-export JAVA_HOME=/usr
-export MAVEN_HOME=$HOME/.m2
-export CLASSPATH=/opt/republic/target/republic-0.6.2.jar:/opt/rep_repository/castor/castor/0.9.5.3/castor-0.9.5.3.jar:/opt/rep_repository/xerces/xercesimpl/2.6.2/xercesImpl-2.6.2.jar:/opt/rep_repository/jdom/jdom/1.0/jdom-1.0.jar:/opt/rep_repository/jaxen/jaxen/1.1-beta-8/jaxen-1.1-beta-8.jar
-
-echo $CLASSPATH
-export REPUBLICSTART=org.republic.Start
-#export OOCALC=oocalc2
--- src/scripts/runExample1.bat
+++
@@ -1,5 +0,0 @@
-rem Test RunExample1.bat
-cd ..
-java -cp lib/republic-0.5.jar;lib/castor-0.9.5.3.jar;lib/xercesImpl.jar;lib/xmlParserAPIs.jar org.republic.Start TestData/TestReport001.rpt TestData/Example1.sxc TestData/Example1Parse.xml NONE
-"C:\Program Files\OpenOffice.org1.1.1\program\soffice.exe" -show -quickstart TestData/Example1.sxc
-cd shellscripts
